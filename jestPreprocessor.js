d0c29b0c5af73c1c6b5a8a849e98fca560e075d7
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-01 17:35:21 +0900

    jest 로  react 테스트 실패 코드 작성 성공, babel 컴파일 작업 성공

--- jestPreprocessor.js
+++ jestPreprocessor.js
@@ -4,6 +4,9 @@ var babel = require("babel-core");
 exports.process = function(sourceText, sourcePath) {
 	console.log(`text : ${sourceText}`);
 	console.log(`path : ${sourcePath}`);
-	compileCode = babel.transform(sourceText);
-	console.log(`compileCode : ${compileCode}`);
+	compileCode = babel.transform(sourceText, {
+		presets : ["es2015", "react"]
+	});
+	console.log(`compileCode : ${compileCode.code}`);
+	return compileCode.code;
 };
