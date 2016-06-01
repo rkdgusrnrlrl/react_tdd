ada58c26579ab5d5cd25c7118ca760baecfa76cb
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-01 17:37:11 +0900

    console.log 코드 삭제

--- jestPreprocessor.js
+++ jestPreprocessor.js
@@ -2,11 +2,8 @@
 var babel = require("babel-core");
 
 exports.process = function(sourceText, sourcePath) {
-	console.log(`text : ${sourceText}`);
-	console.log(`path : ${sourcePath}`);
 	compileCode = babel.transform(sourceText, {
 		presets : ["es2015", "react"]
 	});
-	console.log(`compileCode : ${compileCode.code}`);
 	return compileCode.code;
 };
