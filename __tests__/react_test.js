aa23640877d4d0bbf2cf438378af7c514d64fd2d
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-03 11:30:38 +0900

    Form 컴포넌트 타입 체크 테스트 작성

--- __tests__/react_test.js
+++ __tests__/react_test.js
@@ -21,12 +21,13 @@ describe('개인 정보(이름, 나이, 성별)를 form 으로 서버에 보냄 
     )
     expect(TestUtils.isCompositeComponent(form)).toBeTruthy();
   })
-/*  it('Form 컴포넌트 form 타입', () => {
+  it('Form 컴포넌트 form 타입', () => {
     const form = TestUtils.renderIntoDocument(
       <InsertPersonInfoForm />
     )
     expect(TestUtils.isElementOfType(form, <form></form>)).toBeTruthy();
   })
+  /*
   it('Form 컴포넌트 input 3개 포함', () => {
     const form = TestUtils.renderIntoDocument(
       <InsertPersonInfoForm />
