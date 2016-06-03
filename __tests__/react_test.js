bd6f19ab31a286b0453b534ef7bf9458d9de4cba
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-03 11:27:08 +0900

    react 테스트 수정 개인정보를 보내는 form test

--- __tests__/react_test.js
+++ __tests__/react_test.js
@@ -2,20 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-var CheckboxWithLabel = React.creatClass
 
-describe('CheckboxWithLabel', () => {
-  it('changes the text after click', () => {
-    TestUtils.renderIntoDocument(
-      <CheckboxWithLabel labelOn="On" labelOff="Off" />
+//@todo 개인 정보(이름, 나이, 성별)를 form 으로 서버에 보냄 
+
+describe('개인 정보(이름, 나이, 성별)를 form 으로 서버에 보냄 ', () => {
+  it('Form 컴포넌트 렌더링 input 3개 포함', () => {
+    const form = TestUtils.renderIntoDocument(
+      <InsertPersonInfoForm />
     )
+    expect(TestUtils.isCompositeComponent(form)).toBeTruthy();
   })
+/*  it('Form 컴포넌트 form 타입', () => {
+    const form = TestUtils.renderIntoDocument(
+      <InsertPersonInfoForm />
+    )
+    expect(TestUtils.isElementOfType(form, <form></form>)).toBeTruthy();
+  })
+  it('Form 컴포넌트 input 3개 포함', () => {
+    const form = TestUtils.renderIntoDocument(
+      <InsertPersonInfoForm />
+    )
+    expect(TestUtils.isElementOfType(form, <form></form>)).toBeTruthy();
+  })*/
 })
 
-var CheckboxWithLabel = React.createClass({
-    render : function (){
-        return (
-            <div></div>
-        );
-    }
-})
