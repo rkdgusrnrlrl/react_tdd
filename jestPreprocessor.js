b79f98bd92a7a52a98880065e6cc8f4939686d05
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-03 17:07:25 +0900

    기존 테스트 코드 삭제 jest 테스트코드 및 프로세스 삭제

--- jestPreprocessor.js
+++
@@ -1,9 +0,0 @@
-
-var babel = require("babel-core");
-
-exports.process = function(sourceText, sourcePath) {
-	compileCode = babel.transform(sourceText, {
-		presets : ["es2015", "react"]
-	});
-	return compileCode.code;
-};
