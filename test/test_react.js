34971280fc4ad5a855f21a978760d2633cd34fb1
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-07 18:44:58 +0900

    컴포넌트 테스트지 값이 숫자인경우 contains 메서드 작동하지 않음 디버깅이 겁나 느려서 contains 디버깅해보지 못함ㅠㅠ

--- test/test_react.js
+++ test/test_react.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { mount } from 'enzyme';
 
 /*
 describe("A suite", function() {
@@ -15,7 +15,7 @@ describe("A suite", function() {
   });
 
   it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).is('.foo')).to.equal(true);
+    expect(shallow(<Foo />).is('.foo')).to.equal(true);0-
   });
 
   it("contains spec with an expectation", function() {
@@ -23,22 +23,64 @@ describe("A suite", function() {
   });
 });*/
 
+
+
+
+var mockData = {
+                  id : 1,
+                  payDate:"2016-01-01", 
+                  content:"안녕하세요", 
+                  inOut:"in", 
+                  money:"1000"
+                }
+
 //@ todo HomeKeeperTable 테스트
 //1. HomeKeeperTable 은 table element 여야 함
 //2. className(class)는 테이블 이여야함 (부트스트랩 클래스)
+//3. row 렌더링 하기 
 describe("A suite for HomeKeeperTable", function() {
   var HomeKeeperTable = React.createClass({
     render : function () {
-      return <table className="table"/>
+      var rows = this.props.data.map(function(homekeeper) {
+        return (
+            <tr>
+                <td>{homekeeper.id}</td>
+                <td>{homekeeper.payDate}</td>
+                <td>{homekeeper.content}</td>
+                <td>{homekeeper.inOut}</td>
+                <td>{homekeeper.money}</td>
+            </tr>)
+      });
+
+      return (<table className="table">
+                <tbody>
+                {rows}
+                </tbody>
+              </table>)
     }
   })
+  
 
-  it("HomeKeeperTable 은 table 엘리먼트임", function() {
+  /*it("HomeKeeperTable 은 table 엘리먼트임", function() {
     expect(shallow(<HomeKeeperTable />).is('table')).to.equal(true);
   });
 
   it("className(class)는 테이블 이여야함 (부트스트랩 클래스)", function() {
     expect(shallow(<HomeKeeperTable />).is('.table')).to.equal(true);
+  });*/
+
+  it("row 렌더링 하기", function() {
+    const homekeeper = mount(<HomeKeeperTable data={[mockData]} />);
+    expect(homekeeper.contains(
+        <tr>
+          <td>1</td>
+          <td>2016-01-01</td>
+          <td>안녕하세요</td>
+          <td>in</td>
+          <td>1000</td>
+        </tr>
+      )
+    ).to.equal(true);
   });
   
 });
